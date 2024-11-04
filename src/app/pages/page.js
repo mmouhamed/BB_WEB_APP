8c5a6ef78535bfb2582bbbee4f6b2410ad4b95a6
Mouhari Mouhamed <mmouhamed@healthemed.com>
2024-11-04 10:33:49 -0600

    Iteration 1: Basic Login Page

---
+++ src/app/pages/page.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+export default function HomePage() {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen bg-gradient-to-r from-yellow-500 to-yellow-700 ">
+      {/* // <div className="flex flex-col justify-center items-center min-h-screen bg-gradient-to-r from-green-300 to-blue-300 "> */}
+      <h1 className="text-4xl font-bold text-center text-white mb-4 ">
+        Welcome to BusyBee
+      </h1>
+      <h2 className="text-lg text-center text-white mb-2  ">
+        Work order System - Environmental Services
+      </h2>
+      <p className="text-lg text-center text-white mb-6">
+        Safe, Healthy, Stylish, and Professional
+      </p>
+      <Link href="/pages/user">
+        <button className="bg-black text-white px-6 py-3 rounded-lg border shadow-lg hover:bg-indigo-700 transition duration-300 ">
+          Submit Work Order
+        </button>
+      </Link>
+    </div>
+  );
+}
---
+++ src/app/pages/user/page.js
@@ -0,0 +1,34 @@
+import Link from "next/link";
+// import { useState } from "react";
+
+export default function Login() {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-center mb-4">Login</h1>
+      <form className="bg-white p-8 rounded-lg">
+        <input
+          type="text"
+          className="w-full h-12 border-gray-300 border px-4 mb-4"
+          placeholder="Username"
+          required
+        />
+        <input
+          type="text"
+          className="w-full h-12 border-gray-300 border px-4 mb-4"
+          placeholder="Password"
+          required
+        />
+        <button className="w-full h-12 font-semibold bg-indigo-600 text-white rounded hover:bg-indigo-700 transition duration-300">
+          Login
+        </button>
+        <Link
+          href="/"
+          className="flex justify-center items-center text-gray-900 font-medium mt-4"
+        >
+          Don't have an account?
+          <span className="text-indingo-600 font-semibold pl-2">Sign Up</span>
+        </Link>
+      </form>
+    </div>
+  );
+}
